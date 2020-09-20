a2d9fdf0fbee423764dd6ec5f38e2e3f86066e05
RahulRaj <rajr97333@gmail.com>
2020-09-20 07:45:27 +0200

    Clean some code

--- Legal/api/serializers.py
+++ Legal/api/serializers.py
@@ -27,48 +27,17 @@ class LegalDocuments(serializers.ModelSerializer):
         model = models.Documents
         exclude = ["deleted_at", "timestamp","file"]
 
-
-
-    # def get_file(self, obj):
-    #     if obj.file:
-    #         return self.context["request"].build_absolute_uri(obj.file.url)
-
-    # def create(self, validated_data):
-    #     validated_data["created_by"] = self.context["request"].user
-    #     return super().create(validated_data)
-    
-    # Get File context as text.
     def get_file_text(self, obj):
         if obj.file:
             return text_file(obj.file)
 
         
-
     def get_fileter_text(self, obj):
         listt =[]
         for country in pycountry.countries:
             if country.name in obj.text:
-                #print(country.name)
-                #country = {"Country": [country.name]}
                 df = GetConti(country.name)
                 listt.append((country.name,df))
-                #TBH for testing
-                #list.append(df)
-                #count.append(country.name)
-                # print(text.replace(country.name, df) for n in text)
-                #print(df)
-        # if not listt:
-        #     return obj.text
-        # else:
-        #     text2 = multisub(listt, obj.text )
-        #     list1 =[]
-        #     for citis in pycountry.subdivisions:
-        #         if citis.name in text2:
-        #             for count in  COUNTRY[COUNTRY1]:
-        #                 if citis.name in count:
-        #                     list1.append((citis.name,count))
-        #                  else: 
-        #                     pass
         if not listt:
             return obj.text
         else:
