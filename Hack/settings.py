6fe6b7355bae379b0ac5327cc575559dc1ba6444
RahulRaj <rajr97333@gmail.com>
2020-09-20 02:17:19 +0200

    update requirment.txt

--- Hack/settings.py
+++ Hack/settings.py
@@ -210,5 +210,9 @@ JET_APP_INDEX_DASHBOARD = "jet.dashboard.dashboard.DefaultAppIndexDashboard"
 
 
 
-CLOUDINARY_STORAGE = CLOUDINARY_STORAGE
-DEFAULT_FILE_STORAGE = DEFAULT_FILE_STORAGE
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": "hj4su834i",
+    "API_KEY": "853146674826882",
+    "API_SECRET": "eio5dgL1U45vG3yZ2fIil-xyXJQ",
+}
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
